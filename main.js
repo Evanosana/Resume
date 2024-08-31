e7d9bc36f0fd60bce4153e96d92fad0cd1866148
Evanosana <evanfennern44@gmail.com>
2024-08-31 10:34:51 -0700

    fixed troll buttons and added specific troll classes with specificstyling, also fixed javascript

--- main.js
+++ main.js
@@ -2,24 +2,32 @@ const trollOnButton = document.getElementById("troll-on-button")
 const trollOffButton = document.getElementById("troll-off-button")
 const picture1 = document.getElementById("picture1")
 const picture2 = document.getElementById("picture2")
+const body1=document.getElementById("body1")
+const paper=document.getElementById("paper")
 
+trollOffButton.addEventListener("click", () => {
+    picture1.classList.add("hidden")
+    picture1.classList.remove("img")
 
+    picture2.classList.add("hidden")
+    picture2.classList.remove("dots","img")
 
-trollOnButton.addEventListener("click", () =>{
-    console.log("images should appear")
-    picture1.classList.add("img","dots")
-    picture1.classList.remove("hidden")
+    body1.classList.add("body-fix")
 
-    console.log(picture1.classList)
+    paper.classList.remove("paper-troll")
+})
+trollOnButton.addEventListener("click", () => {
+    picture1.classList.add("img")
+    picture1.classList.remove("hidden","paper-fix")
+
     picture2.classList.add("img","dots")
     picture2.classList.remove("hidden")
-})
-trollOffButton.addEventListener("click", () =>{
-    console.log("images should disappear")
-    picture1.classList.add("hidden")
-    picture1.classList.remove("not-hidden")
 
-    console.log(picture1.classList)
-    picture2.classList.add("hidden")
-    picture2.classList.remove("not-hidden")
+    body1.classList.add("body-troll")
+    body1.classList.remove("body-fix")
+
+    paper.classList.add("paper-troll")
 })
+window.onload = function() {
+    trollOffButton.click();
+}
